38bba5f5ef3c93cd580f8ff2f4d87a31f046403c
tuanhust <tuanhust2010@gmail.com>
2017-02-28 17:17:32 +0700

    push code

---
+++ Up+/Up+/Model/FIRDatabaseManager.swift
@@ -0,0 +1,13 @@
+//
+//  FIRDatabaseManager.swift
+//  Up+
+//
+//  Created by Dreamup on 2/28/17.
+//  Copyright © 2017 Dreamup. All rights reserved.
+//
+
+import UIKit
+
+class FIRDatabaseManager: NSObject {
+
+}
